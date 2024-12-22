56bf5d9ed8fed659f3aa001d7c44e40e56e398a9
Olegbornik <olegbarmotin71@gmail.com>
2024-12-22 13:35:49 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = 'Oleg'
+print(name, type('name'))
+age = 53
+print(name, type(age))
+new_age = 56
+print(name, (new_age))
+print(type(age + new_age))
+print(age + new_age)
+is_student = True
+print(name, type(True))
